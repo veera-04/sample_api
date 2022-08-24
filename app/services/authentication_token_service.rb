2f9e4db66afb80228c5790bff3368eb35c90d01c
Veera <veera4721@gmail.com>
2022-08-24 10:55:32 +0530

    added authentication_service

--- app/services/authentication_token_service.rb
+++ app/services/authentication_token_service.rb
@@ -1,2 +1,8 @@
 class AuthenticationTokenService
+        HMAC_SECRET = 'my$ecretK3y'
+        ALGORITHM_TYPE = 'HS256'
+        def self.call
+                payload = {"data" => "test"}
+                JWT.encode payload, HMAC_SECRET, ALGORITHM_TYPE
+        end
 end
--- spec/services/authentication_token_service_spec.rb
+++ spec/services/authentication_token_service_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 describe AuthenticationTokenService do
         describe '.call' do
+                let(:token){described_class.call}
                 it 'returns authentication token' do
+                        decoded_token = JWT.decode token,described_class::HMAC_SECRET,true,{ algorithm: described_class::ALGORITHM_TYPE }
+                        expect(decoded_token).to eq(
+                                [
+                                        {"data"=>"test"},
+                                        {"alg"=>"HS256"}
+                                ]
+                        )
                 end
         end
 end
